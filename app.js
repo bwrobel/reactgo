826f5a18fef2d8aa3688bcfe3a3dc657a5b888f3
bwrobel <bwrobel@mediaingenuity.com>
2017-01-01 23:36:23 +0000

    functional channels list

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-let channels = [
-    {name:'Hardware Support'},
-    {name:'Software Support'}
-]
-    
-
 class Channel extends React.Component {
     onClick() {
         console.log('I was clicked', this.props.name);
@@ -31,4 +25,70 @@ class ChannelList extends React.Component {
     }
 }
 
-ReactDOM.render(<ChannelList channels={channels} />, document.getElementById('app'));
+class ChannelForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    onSubmit(e) {
+        let channelName = this.state.channelName;
+        console.log(channelName);
+        this.setState({
+            channelName: ''
+        })
+        this.props.addChannel(channelName);
+
+        e.preventDefault();
+    }
+
+    onChange(e){
+        this.setState({
+            channelName: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input 
+                    onChange={this.onChange.bind(this)} type='text'
+                    value={this.state.channelName}
+                    />
+
+            </form>
+        )
+    }
+
+
+}
+
+class ChannelSection extends React.Component {
+    constructor(state){
+        super(state);
+        this.state = {
+            channels:[
+                {name:'Hardware Support'},
+                {name:'Software Support'}
+            ]
+        }
+    }
+
+    addChannel(name){
+        let {channels} = this.state;
+        channels.push({name: name});
+        this.setState({
+            channels:channels
+        });
+    }
+    render() {
+        return (
+            <div>
+                <ChannelList channels={this.state.channels} />
+                <ChannelForm addChannel={this.addChannel.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ChannelSection />, document.getElementById('app'));
